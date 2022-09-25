5bb9756c4f854a54d69f44781678c7621bb3a11b
AlexF99 <alexandreopf@gmail.com>
2022-09-25 16:14:43 -0300

    added add-btn component

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -6,9 +6,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AuthModule } from '@app/auth';
 import { ShellComponent } from './shell.component';
 import { HeaderComponent } from './header/header.component';
+import { AddBtnComponent } from './add-btn/add-btn.component';
 
 @NgModule({
   imports: [CommonModule, NgbModule, AuthModule, RouterModule],
-  declarations: [HeaderComponent, ShellComponent],
+  declarations: [HeaderComponent, ShellComponent, AddBtnComponent],
 })
 export class ShellModule {}
