cb1c825a086d5a6fc10dd5771e97c024d4443d63
AlexF99 <alexandreopf@gmail.com>
2022-10-04 11:48:06 -0300

    verifying server side auth in auth guard

--- src/app/auth/authentication.guard.ts
+++ src/app/auth/authentication.guard.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { HttpService } from '@app/@shared/http.service';
 
 import { Logger } from '@shared';
+import { isThisISOWeek } from 'date-fns';
 import { CredentialsService } from './credentials.service';
 
 const log = new Logger('AuthenticationGuard');
@@ -10,10 +12,21 @@ const log = new Logger('AuthenticationGuard');
   providedIn: 'root',
 })
 export class AuthenticationGuard implements CanActivate {
-  constructor(private router: Router, private credentialsService: CredentialsService) {}
+  constructor(
+    private router: Router,
+    private credentialsService: CredentialsService,
+    private httpService: HttpService
+  ) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     if (this.credentialsService.isAuthenticated()) {
+      this.httpService.getUserInfo().subscribe({
+        next: () => {},
+        error: () => {
+          this.credentialsService.setCredentials();
+        },
+        complete: () => console.info('complete'),
+      });
       return true;
     }
 
